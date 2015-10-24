ce474be2eec68c71678a08ca9b1a9bc80f1ce69a
code1up <dev.code1up@googlemail.com>
2015-10-24 19:07:10 +0100

    wire up backend to police and parking services

--- backend/worker.js
+++ backend/worker.js
@@ -3,16 +3,36 @@
 
 var eyes = require("eyes");
 var Firebase = require("Firebase");
+
 var ParkingService = require("./ParkingService");
+var PoliceService = require("./PoliceService");
 
 var firebaseUrl = "https://piedparker.firebaseio.com/locations/";
 var rootRef = new Firebase(firebaseUrl);
 
 var parkingService = new ParkingService();
+var policeService = new PoliceService();
 
 rootRef.on("child_added", function(snapshot, prevChildKey){
   var locationVal = snapshot.val();
+  var locationRef = snapshot.ref(); 
   var location = locationVal.location;
+
+  policeService.getAllCrimeStats(location)
+    .then(function (data) {
+      if (data.length) {
+        eyes.inspect(data);
+        
+        locationRef.child("crime").set(data, function (error) {
+          if (error) {
+            eyes.inspect(error);          
+          }
+        });
+      }
+    })
+    .catch(function (error) {
+      console.dir(error);
+    });
 
   parkingService.getParkingStats(location)
     .then(function (data) {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* global process */
 var express = require("express");
-var md5 = require("md5")
+var md5 = require("md5");
 var app = express();
 var Firebase = require("firebase");
 var port = process.env.PORT || 1337;
@@ -11,22 +11,26 @@ app.get("/", function (req, res) {
   res.redirect("/index.html");
 });
 
-app.get("/api/_parking/", function (req, res) {
-  
-});
-
 app.get("/api/parking/", function (req, res) {
   var lat = req.query.lat;
   var lon = req.query.lon;
+  var hash = md5(lat + ":" + lon);
 
-  var firebaseurl = "https://piedparker.firebaseio.com/locations/" + md5(lat + ":" + lon);
+  var firebaseLocationUrl = "https://piedparker.firebaseio.com/locations/" + hash;
+  var firebase = new Firebase(firebaseLocationUrl);
 
-  var initFirebase = new Firebase(firebaseurl);
-
-  initFirebase.set({"crime":{},"parking":{},
-    "location":{"lat":lat, "lon":lon}});
-
-  res.json({"url":firebaseurl})
+  firebase.set({ 
+    "crime": {},
+    "parking": {},
+    "location": {
+      "lat": lat,
+      "lon": lon
+    }
+  });
+  
+  res.json({
+    "url": firebaseLocationUrl
+  });
 });
 
 var server = app.listen(port, function () {
