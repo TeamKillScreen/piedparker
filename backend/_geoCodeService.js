31ec8e91b2d477fcad9b3b4169f929f119eeaed7
code1up <dev.code1up@googlemail.com>
2015-10-24 23:13:57 +0100

    added geo coding service

---
+++ backend/_geoCodeService.js
@@ -0,0 +1,17 @@
+var GeoCodeService = require("./geoCodeService");
+var eyes = require("eyes");
+
+var location = {
+	lat: 53.4800,
+	lon: -2.2400	
+};
+
+var geoCodeService = new GeoCodeService();
+
+geoCodeService.reverseGeoCode(location)
+	.then(function (data) {
+		eyes.inspect(data);
+	})
+	.catch(function (error) {
+		console.dir(error);
+	});
---
+++ backend/geoCodeService.js
@@ -0,0 +1,64 @@
+/* jshint -W097 */
+"use strict";
+
+/* global module */
+/* global process */
+/* global require */
+
+// node_modules
+var eyes = require("eyes");
+var Promise = require("Promise");
+var request = require("request");
+var util = require("util");
+
+function reverseGeoCode (location) {
+	return new Promise(function (resolve, reject) {
+        var headers = {
+            "content-type": "application/x-www-form-urlencoded"
+        };
+        
+        var options = {
+            url: "https://api.postcodes.io/postcodes",
+            method: "POST",
+            headers: headers,
+            form: {
+                geolocations: [{
+                    longitude: location.lon,
+                    latitude: location.lat,
+                }]
+            }
+        };
+        
+        eyes.inspect(options);    
+        
+        request(options, function (error, response, data) {
+            if (error) {
+                reject(error);
+                return;
+            }
+        
+            if (response.statusCode !== 200) {
+                reject(response.statusCode);
+                return;
+            }
+
+            var result = JSON.parse(data).result;
+
+            eyes.inspect(result);
+    
+            if (!result.length) {
+                reject("No result.");
+                return;            
+            }
+            
+            resolve(result[0]);
+        });
+	});
+}
+
+function GeoCodeService () {	
+}
+
+GeoCodeService.prototype.reverseGeoCode = reverseGeoCode;
+
+module.exports = GeoCodeService;
