1607fe12ac987891519ad51d746e3471f6273d56
Craig Reay <craigcsruk@googlemail.com>
2015-10-24 15:20:17 +0100

    ReactFire implementation

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,8 +1,26 @@
-var CarPark = React.createClass({displayName: 'CarPark',
+var CarPark = React.createClass({displayName: 'test',
+        mixins: [ReactFireMixin],
+        getInitialState: function() {
+                return {
+                        test: []
+                };
+        },
+        componentWillMount: function() {
+                var firebaseRef = new Firebase('https://piedparker.firebaseio.com/test/');
+                this.bindAsArray(firebaseRef.limitToLast(25), 'test');
+        },
 	render: function() {
+                var carparks = this.state.test.map(function(carpark, index) {
+                        return (
+                        <h1 key={index}>{carpark.name}</h1>
+                        )
+                });
 		return (
-			<h1>Hello world?!</h1>
-			);
+                        <div>
+			     <h1>Hello world?!</h1>
+                             {carparks}
+                        </div>
+		);
 	}
 });
 
