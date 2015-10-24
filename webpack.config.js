08be1aa814a8866c6fd1ef9cc0cfac990580a67f
Craig Reay <craigcsruk@googlemail.com>
2015-10-24 17:21:02 +0100

    Added webpack config and the ability to do npm run start-dev

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: "./app/src/app.jsx",
+	output: {
+		filename: "./app/src/app.js"
+	},
+	module: {
+	loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel?optional[]=runtime&stage=0'
+			}
+		]
+	}
+}
