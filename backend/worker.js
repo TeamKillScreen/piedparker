6804184c3e345d0636eee7fb4a5de1fe547dd86f
Richard Pearson <github@catdevnull.co.uk>
2015-10-24 23:21:01 +0100

    Worker now refreshes parking data every 5 seconds.

--- backend/worker.js
+++ backend/worker.js
@@ -16,19 +16,33 @@ var rootRef = new Firebase(firebaseUrl);
 var parkingService = new ParkingService();
 var policeService = new PoliceService();
 
-  
-rootRef.on("child_added", function (snapshot) {
-  function setLocationStats () {
-    var locationVal = snapshot.val();
-    var locationRef = snapshot.ref(); 
-    var location = locationVal.location;
-  
-    policeService.getAllCrimeStatsAndAnalysis(location)
+function updateParking(location, snapshot)
+{
+  parkingService.getParkingStats(location)
+    .then(function (data) {
+      if (data.length) {
+        //eyes.inspect(data);
+        
+        snapshot.ref().child("parking").set(data, function (error) {
+          if (error) {
+            eyes.inspect(error);          
+          }
+        });
+      }
+    })
+    .catch(function (error) {
+      console.dir(error);
+    });
+}
+
+function updateCrimeData(location, snapshot)
+{
+  policeService.getAllCrimeStatsAndAnalysis(location)
       .then(function (data) {
         if (data) {
-          eyes.inspect(data);
+          //eyes.inspect(data);
           
-          locationRef.child("crime").set(data, function (error) {
+          snapshot.ref().child("crime").set(data, function (error) {
             if (error) {
               eyes.inspect(error);          
             }
@@ -40,24 +54,33 @@ rootRef.on("child_added", function (snapshot) {
       .catch(function (error) {
         console.dir(error);
       });
+}
+
   
-    parkingService.getParkingStats(location)
-      .then(function (data) {
-        if (data.length) {
-          eyes.inspect(data);
-          
-          snapshot.ref().child("parking").set(data, function (error) {
-            if (error) {
-              eyes.inspect(error);          
-            }
-          });
-        }
-      })
-      .catch(function (error) {
-        console.dir(error);
-      });  
+rootRef.on("child_added", function (snapshot) {
+  function setLocationStats () {
+    var locationVal = snapshot.val();
+    var locationRef = snapshot.ref(); 
+    var location = locationVal.location;
+  
+    updateCrimeData(location, snapshot);
+  
+    updateParking(location, snapshot);
   }
 
   setLocationStats();  
-  // setInterval(setLocationStats, 15 * 1000);
+  
 });
+
+setInterval(function (){
+  console.log("Updating Parking Data")
+  rootRef.once("value",function(dataSnapshot) {
+    dataSnapshot.forEach(function(childSnapshot) {
+      var locationVal = childSnapshot.val();
+
+      var location = locationVal.location;
+
+      updateParking(location, childSnapshot);
+    });
+  });
+}, 5000);
