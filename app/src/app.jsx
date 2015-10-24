7bf8b4a8c46fc3184789a57fe5decd14d0b13271
JMonono <julian.monono@gmail.com>
2015-10-24 15:55:46 +0100

    Added carpark.jsx file and separate component

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,30 +1,45 @@
-var CarPark = React.createClass({displayName: 'test',
-        mixins: [ReactFireMixin],
-        getInitialState: function() {
-                return {
-                        test: []
-                };
-        },
-        componentWillMount: function() {
-                var firebaseRef = new Firebase('https://piedparker.firebaseio.com/test/');
-                this.bindAsArray(firebaseRef.limitToLast(25), 'test');
-        },
+var CarPark = React.createClass(
+  {
+    render: function(){
+      return(
+        <h1>
+          {this.props.details.name}
+        </h1>
+      )
+    }
+  }
+);
+
+var Main = React.createClass({displayName: 'test',
+	mixins: [ReactFireMixin],
+	getInitialState: function() {
+		return {
+			test: []
+		};
+	},
+	componentWillMount: function() {
+		var firebaseRef = new Firebase('https://piedparker.firebaseio.com/test/');
+		this.bindAsArray(firebaseRef.limitToLast(25), 'test');
+	},
 	render: function() {
-                var carparks = this.state.test.map(function(carpark, index) {
-                        return (
-                        <h1 key={index}>{carpark.name}</h1>
-                        )
-                });
+		var carparks = this.state.test.map(function(carpark, index) {
+			return (
+				<CarPark key={index} details={carpark} />
+			)
+		});
 		return (
-                        <div>
-			     <h1>Hello world?!</h1>
-                             {carparks}
-                        </div>
+			<div>
+
+				<h1>
+					Hello world?!
+				</h1>
+				{carparks}
+			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	React.createElement(CarPark, null),
+	React.createElement(Main, null),
 	document.getElementById('Main')
-	);
+);
